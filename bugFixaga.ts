287a17978720a373f2c80dee8b34d384085f64d3
Krystian Kat <krystian.kat@protonmail.com>
2022-03-27 11:02:20 +0200

    feat: add best implementation to fix all bugs

---
+++ bugFixaga.ts
@@ -0,0 +1,8 @@
+export const bugFixaga = (number: number) => {
+  const iFixaYourBufa = (howManyBuga: number) => {
+    for (let i = 0; i < howManyBuga; i++) {
+      console.log('Ugabuga fixta buga!');
+    }
+    return console.log('Buga Fixtada!');
+  };
+};
