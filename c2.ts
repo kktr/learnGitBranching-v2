99e1cf9a73a6b001e34fc6749482f712127d1278
Krystian Kat <krystian.kat@protonmail.com>
2022-04-02 17:12:40 +0200

    feat: add calcDifferenceInDaysBetweenTwoDays

---
+++ c2.ts
@@ -0,0 +1,13 @@
+// c2
+
+function calcSumOfTwoNumbers(a, b) {
+  return a + b;
+}
+
+function calcDifferenceInDaysBetweenTwoDays(a: Date, b: Date) {
+  return Math.abs(a.getTime() - b.getTime()) / (1000 * 3600 * 24);
+}
+
+console.log(
+  calcDifferenceInDaysBetweenTwoDays(new Date(2020, 1, 1), new Date(2021, 1, 1))
+);
